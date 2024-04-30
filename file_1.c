c2bce76e0085645c48c180cec18fbbd3b0e80ef3
2020wa86587@wilp.bits-pilani.ac.in <2020wa86587@wilp.bits-pilani.ac.in>
2024-04-30 18:15:33 +0530

    adding some function from main branch

--- file_1.c
+++ file_1.c
@@ -7,4 +7,10 @@ Scanf("%d %d", &number1, &number2);
 sum=number1 + number2;
 printf("%d + %d = %d", number1, number2, sum);
 return 0;
-}
+<<<<<<< HEAD
+	int addfunc(int a, int b);
+int c = a+b;
+return c;
+//function added in branch1 int func_add(int a, int b)
+return a+b;
+>>>>>>>branch1
